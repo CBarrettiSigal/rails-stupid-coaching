a319311a3edcfc4a99c17cbaf23b40e2e9a8d018
Christina Barreti Sigal <CBarrettiSigal@gmail.com>
2020-02-17 13:07:20 -0500

    partway through stupid coaching Week 6 Day 1

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,17 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @question = params[:question]
+    @answer = params[:answer]
+    correct = "I am going to work right now!"
+
+    # access question from params
+    if @question == correct
+      @answer = "Ok great, see ya!"
+    elsif @question !=  correct
+      @answer = "I don't care! Get dressed and get to work!"
+    end
+  end
+end
