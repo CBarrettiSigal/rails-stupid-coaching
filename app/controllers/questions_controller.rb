03583a38096b3a5080fc7edcf1aeaf49ef98e8e5
Christina Barreti Sigal <CBarrettiSigal@gmail.com>
2020-02-17 14:29:56 -0500

    For real this time, finishing up stupid coaching exercise

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,12 +5,12 @@ class QuestionsController < ApplicationController
   def answer
     @question = params[:question]
     @answer = params[:answer]
-    correct = "I am going to work right now!"
+    correct = 'I am going to work right now!'
 
     # access question from params
     if @question == correct
-      @answer = "Ok great, see ya!"
-    elsif @question !=  correct
+      @answer = 'Ok great, see ya!'
+    elsif @question != correct
       @answer = "I don't care! Get dressed and get to work!"
     end
   end
